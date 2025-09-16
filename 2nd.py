ce6de370240be7ca547b98ee93f995fdc620b09b
KhushiCodes02 <khushisahuu.005@gmail.com>
2025-09-16 14:42:35 +0530

    first commit

---
+++ 2nd.py
@@ -0,0 +1,12 @@
+from flask import Flask , render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("1.html")
+
+@app.route("/about")
+def prince():
+    name = "Harsh_Sahu"
+    return render_template("2.html" , name2 = name)
+app.run(debug=True)
